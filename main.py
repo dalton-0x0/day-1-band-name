d1e31b6830a435ed14b0b0ebecc9ac828c813f1a
dalton-0x0 <12995327+dalton-0x0@users.noreply.github.com>
2023-12-26 07:34:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# welcome text
+print("Welcome to the BAND NAME Generator!")
+
+# input city name
+city = input("What's the name of the city you grew up in?\n")
+
+# input pet name
+pet = input("What's the name of your pet?\n")
+
+# print band name
+print("Your band name could be", city.upper(), pet.upper())
